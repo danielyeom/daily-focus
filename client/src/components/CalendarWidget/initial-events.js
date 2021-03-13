04975e9e6ccb558c34370cece82f8a5755496c8c
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2021-03-13 20:29:25 +1300

    Calendar can create, view, and remove. More to come.
    Calendar widget can create, view, and remove.
    author Daniel Yeom <dyeo713@aucklanduni.ac.nz> 1615620565 +1300
    committer Daniel Yeom <dyeo713@aucklanduni.ac.nz> 1616138625 +1300
    
    Initial commit for the calendar component. Added react-calendar.
    
    Added event view component which just shows the selected date for now.
    
    dummy add-event button. Need to implement adding events to specific days
    
    can show content on the tiles. created arrays for events.
    
    functions for managing events. Update, delete, render. Need to implememt createEventHanlder after.
    
    House keeping
    
    Calendar widget can add, remove, and view events.
    
    Deleted EventView.css. Proper styling coming soon
    
    Initial commit for the calendar component. Added react-calendar.
    
    Added event view component which just shows the selected date for now.
    
    dummy add-event button. Need to implement adding events to specific days
    
    can show content on the tiles. created arrays for events.
    
    functions for managing events. Update, delete, render. Need to implememt createEventHanlder after.
    
    House keeping
    
    changed file names to match our team's conventions
    
    changed the format of the events array to store date objects
    
    moved the initial events to another js file
    
    added event display functionality to the EventView component
    
    trying to add events, not re rendering components for some reason
    
    idk man
    
    I tryied
    
    bruh idk anymore
    
    please work
    
    FIXED
    
    - added npm prop-types. calendar can add and remove events.
    
    Calendar widget can add, remove, and view events.
    
    Deleted EventView.css. Proper styling coming soon
    
    bruhhhhhhhhhhhhhhh
    
    gotta stop cooking the code

---
+++ client/src/components/CalendarWidget/initial-events.js
@@ -0,0 +1,23 @@
+/**
+ * contains some dummy events
+ */
+
+const initialEvents = [
+    {
+        date: new Date("March 2, 2021 02:00:00"),
+        title: "test 1",
+        description: "This is test event one 1",
+    },
+    {
+        date: new Date("March 2, 2021 03:00:00"),
+        title: "test 2",
+        description: "This is test 2, on the same day as test 1",
+    },
+    {
+        date: new Date("March 17, 2021 21:00:00"),
+        title: "test 3",
+        description: "This is test event 3!",
+    },
+];
+
+export default initialEvents;

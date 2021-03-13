1803c0ffe1d81d927b19ecfb48e3d8ff9c97a3cc
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2021-03-13 20:29:25 +1300

    Calendar can create, view, and remove. More to come.
    Calendar widget can create, view, and remove.
    author Daniel Yeom <dyeo713@aucklanduni.ac.nz> 1615620565 +1300
    committer Daniel Yeom <dyeo713@aucklanduni.ac.nz> 1616138625 +1300
    
    Initial commit for the calendar component. Added react-calendar.
    
    Added event view component which just shows the selected date for now.
    
    dummy add-event button. Need to implement adding events to specific days
    
    can show content on the tiles. created arrays for events.
    
    functions for managing events. Update, delete, render. Need to implememt createEventHanlder after.
    
    House keeping
    
    Calendar widget can add, remove, and view events.
    
    Deleted EventView.css. Proper styling coming soon
    
    Initial commit for the calendar component. Added react-calendar.
    
    Added event view component which just shows the selected date for now.
    
    dummy add-event button. Need to implement adding events to specific days
    
    can show content on the tiles. created arrays for events.
    
    functions for managing events. Update, delete, render. Need to implememt createEventHanlder after.
    
    House keeping
    
    changed file names to match our team's conventions
    
    changed the format of the events array to store date objects
    
    moved the initial events to another js file
    
    added event display functionality to the EventView component
    
    trying to add events, not re rendering components for some reason
    
    idk man
    
    I tryied
    
    bruh idk anymore
    
    please work
    
    FIXED
    
    - added npm prop-types. calendar can add and remove events.
    
    Calendar widget can add, remove, and view events.
    
    Deleted EventView.css. Proper styling coming soon
    
    bruhhhhhhhhhhhhhhh
    
    gotta stop cooking the code

---
+++ client/src/components/CalendarWidget/NewEvent/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function NewEvent({ onAddNewEvent }) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const AddEventHandler = () => {
+        onAddNewEvent(title, description);
+        setTitle("");
+        setDescription("");
+    };
+
+    return (
+        <div>
+            <div>
+                <label>New Event</label>
+                <input type="text" value={title} onInput={(e) => setTitle(e.target.value)}></input>
+                <label>Description</label>
+                <input
+                    type="text"
+                    value={description}
+                    onInput={(e) => setDescription(e.target.value)}
+                ></input>
+            </div>
+
+            <button onClick={() => AddEventHandler()}>Add Event</button>
+        </div>
+    );
+}
+
+NewEvent.propTypes = {
+    onAddNewEvent: PropTypes.func,
+};

c48d9f0cafaeb67b68fff61ea856b9293b65cf3e
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2021-03-15 15:45:27 +1300

    can show content on the tiles. created arrays for events.

--- client/src/components/CalendarWidget/CalendarWidget.js
+++ client/src/components/CalendarWidget/CalendarWidget.js
@@ -7,10 +7,35 @@ import EventView from "./EventView/EventView.js";
 export default function CalendarWidget() {
     const [selected, setSelected] = useState(new Date());
 
+    const events = [
+        // {
+        //     id: ,
+        //     date: ,
+        //     title: ,
+        //     description:
+        // }
+        {
+            id: 1,
+            date: 4,
+            title: "test 1",
+            description: "This is test event one 1",
+        },
+        {
+            id: 2,
+            date: 16,
+            title: "test 2",
+            description: "This is test event 2!",
+        },
+    ];
+
+    //renders the array
+    const content = ({ date }) => (
+        <div>{events.map((val) => (val.date === date.getDate() ? val.title : null))}</div>
+    );
     return (
         <div>
-            <div style={{ width: "400px", height: "200px" }}>
-                <Calendar onClickDay={(value) => setSelected(value)} />
+            <div style={{ width: "400px" }}>
+                <Calendar onClickDay={(value) => setSelected(value)} tileContent={content} />
             </div>
             <div>
                 <EventView selected={selected} />

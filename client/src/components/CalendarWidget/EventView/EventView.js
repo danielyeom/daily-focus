92ac7bd7ecd209d3786ec6ad9a64d01f18dc2df6
Michael Pham <tpha585@aucklanduni.ac.nz>
2021-03-17 07:01:24 +1300

    functions for managing events. Update, delete, render. Need to implememt createEventHanlder after.

---
+++ client/src/components/CalendarWidget/EventView/EventView.js
@@ -0,0 +1,66 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+// import "./EventView.css";
+
+const event = (props) => {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    return (
+        <div className="event">
+            <p onClick={props.click}>
+                Date: {props.date}, Month: {months[props.month]}, Title: {props.title}, Description:{" "}
+                {props.description}
+            </p>
+            <button onClick={props.delete}>-</button>
+            {props.show ? (
+                <div>
+                    <input
+                        type="text"
+                        defaultValue="change title"
+                        onChange={props.changeTitle}
+                        value={props.title}
+                    />
+                    <input
+                        type="text"
+                        defaultValue="change description"
+                        onChange={props.changedDescription}
+                        value={props.description}
+                    />
+                </div>
+            ) : null}
+        </div>
+    );
+};
+export default event;
+
+{
+    /* {props.selected.getDate()} {months[props.selected.getMonth()]}
+    {console.log(props)} */
+}
+{
+    /* <input type='text' onChange={props.changed} value={props.name}/> */
+}
+{
+    /* <button onClick={() => console.log("trying to add event for: ", months[props.selected.getMonth()])}>
+    Add Event
+</button> */
+}
+{
+    /* <button onClick>-</button> */
+}
